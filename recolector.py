c85adb5996a3858a48b279c0216cecb447ca26ca
Arlo2004 <manuelgjuan04@gmail.com>
2026-01-17 23:04:01 +0100

    Update recolector.py

--- recolector.py
+++ recolector.py
@@ -4,24 +4,24 @@ from datetime import datetime
 import os
 
 URL_API = "https://portail-api-data.montpellier3m.fr/offstreetparking?limit=1000"
-CSV_FILE = "parkings_montpellier.csv"
-file_exists =   os.path.exists(CSV_FILE)
+CSV_FILE ="parkings_montpellier.csv"
+file_exists =    os.path.exists(CSV_FILE)
 
 try:
     response = requests.get(  URL_API, timeout=15  )
     if response.status_code== 200:
         data = response.json()
-        fecha_lectura = datetime.now().strftime("%Y-%m-%d %H:%M:%S") #corazon
-        with open(CSV_FILE, mode="a",  newline="",  encoding="utf-8") as file:
+        fecha_lectura = datetime.now().strftime(  "%Y-%m-%d %H:%M:%S") #corazon
+        with open (CSV_FILE, mode="a",  newline="",  encoding="utf-8")as file:
             writer = csv.writer(file)
             if not file_exists:
-                writer.writerow(["fecha_lectura",  "nombre_parking", "plazas_libres", "capacidad_total",  "estado"])
+                writer.writerow([ "fecha_lectura", "nombre_parking","plazas_libres","capacidad_total",  "estado"])
             for parking in data:
-                nombre = parking.get( "name", {} ).get("value", "Desconocido")
+                nombre = parking.get( "name", {} ).get( "value","Desconocido"  )
                 plazas_libres = parking.get("availableSpotNumber", {}).get(  "value", 0  )
                 estado = parking.get("status", {}).get("value", "Sin datos")
                 capacidad =  parking.get("totalSpotNumber", {}).get("value",  "0"  )
-                writer.writerow([fecha_lectura, nombre,  plazas_libres, capacidad, estado])
+                writer.writerow([fecha_lectura, nombre, plazas_libres,capacidad, estado])
         print("Lectura exitosa")
     else:
         print("Error API")
